78eaec3d6ad67a0faa0d8da3cb161df72e3c6bf4
KovTon <ikovtonogov@gmail.com>
2023-03-17 10:32:03 +0400

    modified:   example.md 	modified:   ideas.md 	modified:   ivan's_deck.py 	modified:   main.py 	modified:   readme.md

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from typing import List, Tuple
+from typing import List, Dict, Any, Optional
 import random
 
-Card = Tuple[str, str, int]
+Card = Dict[str, Any]
+Player = Dict[str, Any]
 
 
 def get_grade(number: int):
@@ -15,7 +16,7 @@ def generate_deck(suits: List[str], values: List[int]):
     deck = []
     for suit in suits:
         for value in values:
-            card = (get_grade(value), suit, value)
+            card = {'grade': get_grade(value), 'suit': suit, 'value': value}
             deck.append(card)
     return deck
 
@@ -41,59 +42,73 @@ def generate_double_suite_deck():
     return deck
 
 
-def pull_random_card(deck) -> Card:
+def pull_random_card(deck: List[Card]) -> Card:
     card_index = random.randint(0, len(deck)-1)
     card = deck.pop(card_index)
     return card
 
 
 def cards_are_equal(card_1: Card, card_2: Card) -> bool:
-    return card_1[2] == card_2[2]
+    return card_1['value'] == card_2['value']
 
 
-def start_round(deck, player_1, player_2):
+def start_round(deck: List[Card], player_1: Player, player_2: Player):
     player_1_card = pull_random_card(deck)
     player_2_card = pull_random_card(deck)
     # 64 строка была 1-4 главах? =
     if cards_are_equal(player_1_card, player_2_card):
         return
 
-    if player_1_card[2] > player_2_card[2]:
-        player_1.append(player_1_card)
+    if player_1_card['value'] > player_2_card['value']:
+        # player_1.append(player_1_card)
+        # получить руку
+        # непонятно
+        hand_1: List[Card] = player_1['hand']
+        # добавить в руку
+        hand_1.append(player_1_card)
     else:
-        player_2.append(player_2_card)
+        # получить руку
+        hand_2: List[Card] = player_2['hand']
+        # добавить в руку
+        hand_2.append(player_2_card)
 
 
-def introduce_game_settings(deck):
+def introduce_game(deck):
     print(f'вы играете колодой из {len(deck)} карт')
 
 
-def start_game_loop(deck, player_1, player_2):
+def start_game_loop(deck: List[Card], player_1: Player, player_2: Player):
     while deck:  # неявная конвертацияиз list в bool. Явная когда вызвали
         start_round(deck, player_1, player_2)
 
 
-def show_results(player_1, player_2):
-    print(len(player_1), len(player_2))
+def show_results(player_1: Player, player_2: Player):
+    print(len(player_1['hand']), len(player_2['hand']))
 
 
-def anounce_winner(player_1, player_2):
-    if len(player_1) == len(player_2):
-        print("draw")
-    elif len(player_1) > len(player_2):
-        print("winner-player 1")
+def define_winner(player_1: Player, player_2: Player) -> Optional[Player]:
+    if len(player_1['hand']) > len(player_2['hand']):
+        return player_1
     else:
-        print("winner-player 2")
+        return player_2
+    return None
+
+
+def anounce_winner(winner: Optional[Player]) -> None:
+    if winner is None:
+        print('draww')
+    else:
+        print(f'winner {winner["name"]}')
 
 
 def main():
-    player_1 = []
-    player_2 = []
+    player_1 = {'name': 'Ivan', 'hand': []}
+    player_2 = {'name': 'Pavel', 'hand': []}
     deck = generate_standart_deck()
     random.shuffle(deck)
-    introduce_game_settings(deck)
+    introduce_game(deck)
     start_game_loop(deck, player_1, player_2)
-    anounce_winner(player_1, player_2)
+    anounce_winner(define_winner(player_1, player_2))
     show_results(player_1, player_2)
 
 
